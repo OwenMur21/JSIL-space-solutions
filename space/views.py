0a1e802ae0227066dfa1a99ae3fccfed559acab5
Gift-Lumumba <giftlumumba2@gmail.com>
2018-11-29 20:08:36 +0300

    creates comment and like view function to enable image liking and commenting

--- space/views.py
+++ space/views.py
@@ -16,12 +16,17 @@ from django.contrib import messages
 from django.conf import settings
 import requests
 from .decorators import check_recaptcha
+from django.http import JsonResponse
+
+
+
 
 def landing(request):
-    return render(request, 'home.html')
-
-
-
+    images = Image.objects.all()
+    comments = Comment.objects.all()
+    likes = Likes.objects.all()
+    return render(request, 'home.html',locals())
+    
 def signup(request):
 
     if request.method == 'POST':
@@ -66,3 +71,26 @@ def signup(request):
         form = SignupForm()
 
     return render(request, 'registration/registration_form.html', {'form': form})
+
+@login_required(login_url='/accounts/login/')
+def comment(request, image_id):
+    images = get_object_or_404(Image, pk=image_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.image = images
+            comment.save()
+    return redirect('landing')
+
+
+@login_required(login_url='/accounts/login/')
+def like(request, image_id):
+    current_user = request.user
+    liked_image=Image.objects.get(id=image_id)
+    new_like,created= Likes.objects.get_or_create(who_liked=current_user, liked_image=liked_image)
+    new_like.save()
+
+    return redirect('landing')
+
