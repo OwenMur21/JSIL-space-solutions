69a74844141dbf8cf4a8f45c346d423732423fc2
Gift-Lumumba <giftlumumba2@gmail.com>
2018-11-29 12:20:33 +0300

    creates view function for reCAPTCHA validation after signup

--- space/views.py
+++ space/views.py
@@ -1,4 +1,68 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect , get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from django.contrib.auth.models import User # May not be in use
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth import login, authenticate, get_user_model
+import json
+import urllib
+from django.shortcuts import render, redirect,get_object_or_404
+from django.conf import settings
+from django.contrib import messages
+from .forms import *
+from .models import *
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib import messages
+from django.conf import settings
+import requests
+from .decorators import check_recaptcha
 
 def landing(request):
     return render(request, 'home.html')
+
+
+
+def signup(request):
+
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+  
+            ''' Begin reCAPTCHA validation '''
+            recaptcha_response = request.POST.get('g-recaptcha-response')
+            url = 'https://www.google.com/recaptcha/api/siteverify'
+            values = {
+                'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
+                'response': recaptcha_response
+            }
+            data = urllib.parse.urlencode(values).encode()
+            req =  urllib.request.Request(url, data=data)
+            response = urllib.request.urlopen(req)
+            result = json.loads(response.read().decode())
+            ''' End reCAPTCHA validation '''
+
+            if result['success']:
+                form.save()
+                messages.success(request, 'Account verified successfully!')
+            else:
+                messages.error(request, 'Invalid reCAPTCHA. Please try again.')
+
+            return redirect('landing')
+    else:
+        form = SignupForm()
+
+    return render(request, 'registration/registration_form.html', {'form': form})
+
+@check_recaptcha
+def signup(request):
+
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid() and request.recaptcha_is_valid:
+            form.save()
+            messages.success(request, 'Account created successfully!')
+            return redirect('landing')
+    else:
+        form = SignupForm()
+
+    return render(request, 'registration/registration_form.html', {'form': form})
