89c582fefaaaa2ee63e0ec5b97ba33f2f57fac48
Gift-Lumumba <giftlumumba2@gmail.com>
2018-11-29 20:06:41 +0300

    creates Image,Likes and Comment classes

--- space/models.py
+++ space/models.py
@@ -1,3 +1,89 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Image(models.Model):
+    '''
+    class that contains image properties,methods and functions
+    '''
+    post = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=40)
+    posted_on = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+
+    def __str__(self):
+      return self.name
+
+    def save_image(self):
+        self.save
+
+    def delete_image(self):
+        self.delete
+
+    class Meta:
+        ordering = ['posted_on']
+
+    @classmethod
+    def get_all_images(cls):
+        images = cls.objects.order_by()
+        return images
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = Image.objects.filter(user_id=id).all()
+        return image
+
+    @classmethod
+    def search_images(cls,name):
+        image =  cls.objects.filter(name__icontains=name)
+        return image
+
+    @property
+    def count_likes(self):
+        likes = self.likes.count()
+        return likes
+
+
+    @property
+    def count_comments(self):
+        comments = self.comments.count()
+        return comments
+
+class Comment(models.Model):
+        """
+        Class that contains comment details
+        """
+        comment = models.TextField()
+        posted_on = models.DateTimeField(auto_now=True)
+        image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name='comment')
+        user = models.ForeignKey(User, on_delete=models.CASCADE,null="True")
+
+        def __str__(self):
+                return self.comment
+
+        class Meta:
+                ordering = ['posted_on']
+         
+        def save_comment(self):
+                self.save()
+
+        def del_comment(self):
+                self.delete()
+
+        @classmethod
+        def get_comments_by_image_id(cls, image):
+                comments = Comment.objects.get(image_id=image)
+                return comments
+
+
+class Likes(models.Model):
+    who_liked=models.ForeignKey(User,on_delete=models.CASCADE, related_name='likes')
+    liked_image =models.ForeignKey(Image, on_delete=models.CASCADE, related_name='likes')
+
+    def save_like(self):
+        self.save() 
+
+    def __str__(self):
+      return self.who_liked
