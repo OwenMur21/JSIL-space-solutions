cf476ecea84a872ef3ffe155fc9d5d278bd1f9c6
Gift-Lumumba <giftlumumba2@gmail.com>
2018-11-29 12:22:46 +0300

    adds urlpatterns for user registration and social media login

--- jsil/urls.py
+++ jsil/urls.py
@@ -1,7 +1,13 @@
-from django.conf.urls import url, include
+from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include('space.urls')),
+    url(r'', include('space.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
+    url(r'^', include('django.contrib.auth.urls')),
+    url(r'^oauth/', include('social_django.urls',namespace="social")),
+    url(r'^accounts/', include('allauth.urls')),
 ]
