cb7486414839cbaf4d3675e4b5cec1b0db24f5fc
Gift-Lumumba <giftlumumba2@gmail.com>
2018-12-02 12:09:50 +0300

    updates and fixes comment and like url patterns

--- space/urls.py
+++ space/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns=[
   url('^$',views.landing,name = 'landing'),
-  url(r'^comment/(?P<image_id>\d+)', views.comment, name='comment'),
-  url(r'^like/(?P<image_id>\d+)', views.like, name='like'),
+  url(r'^comment/(?P<product_id>\d+)', views.comment, name='comment'),
+  url(r'^like/(?P<product_id>\d+)', views.like, name='like'),
 
 ]
 
